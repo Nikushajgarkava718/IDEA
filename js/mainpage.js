faf207c7734f0634a5df7d7d57d453db8ccceea4
Nikushajgarkava45 <nikushajgarkava3@gmail.com>
2026-08-16 02:48:35 +0400

    fix html css js

--- js/mainpage.js
+++ js/mainpage.js
@@ -2,6 +2,8 @@ function fieldSliderFn() {
   const wrapper = document.querySelector(".field-slider-wrapper");
   const track = document.getElementById("field-cards");
   const cards = document.querySelectorAll(".field-card");
+  const prevBtn = document.getElementById("field-prev");
+  const nextBtn = document.getElementById("field-next");
 
   let activeIndex = 0;
   const totalCards = cards.length;
@@ -19,13 +21,37 @@ function fieldSliderFn() {
     renderSlider();
   }
 
+  function goToPrevCard() {
+    if (activeIndex === 0) {
+      activeIndex = totalCards - 1;
+    } else {
+      activeIndex--;
+    }
+    renderSlider();
+  }
+
   let autoSlideId = setInterval(goToNextCard, 5000);
 
-  wrapper.addEventListener("mouseenter", () => {
+  function stopAutoSlide() {
     clearInterval(autoSlideId);
-  });
-  wrapper.addEventListener("mouseleave", () => {
+  }
+  function startAutoSlide() {
     autoSlideId = setInterval(goToNextCard, 5000);
+  }
+
+  wrapper.addEventListener("mouseenter", stopAutoSlide);
+  wrapper.addEventListener("mouseleave", startAutoSlide);
+
+  nextBtn.addEventListener("click", () => {
+    goToNextCard();
+    stopAutoSlide();
+    startAutoSlide();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    goToPrevCard();
+    stopAutoSlide();
+    startAutoSlide();
   });
 }
 
